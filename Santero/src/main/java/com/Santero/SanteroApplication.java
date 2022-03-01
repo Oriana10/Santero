4e1259b8a07ca740cc7f5eda37b48e4b70bd801b
Santiago Perez <santy.p510@gmail.com>
2022-03-01 18:27:01 -0300

    Project added

---
+++ Santero/src/main/java/com/Santero/SanteroApplication.java
@@ -0,0 +1,13 @@
+package com.Santero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanteroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanteroApplication.class, args);
+	}
+
+}
