f9f27c925b7fd01bf696847332b62e22d40aaa38
Markos <markosmk@me.com>
2022-03-01 17:23:49 -0300

    Add Router base and global state

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// components
+import Layout from './components/layout/Layout';
+import { Login, Registro, Home, Menus, NotFound } from './pages';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="registro" element={<Registro />} />
+          <Route path="menus" element={<Menus />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
