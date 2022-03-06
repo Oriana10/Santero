97881dc50ba006dc1ee7d49692191a3c5257efc6
Markos <markosmk@me.com>
2022-03-05 21:15:10 -0300

    Add Header and Footer with styles

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,5 +1,25 @@
+import logo from '../../assets/logo-nav.png';
+import styles from './Footer.module.css';
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.linksFooter}>
+        <li>Como comprar?</li>
+        <li>Medios de Pago</li>
+        <li>Nosotros</li>
+      </ul>
+
+      <div className={styles.logoFooter}>
+        <img src={logo} alt=" " />
+      </div>
+
+      <div className={styles.redesFooter}>
+        <div>Copyright</div>
+        <div>Redes</div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,15 +1,49 @@
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
 import styles from './Header.module.css';
 
+import logo from '../../assets/logo-nav.png';
+import IconSearch from '../icons/IconSearch';
+import IconShopingCart from '../icons/IconShopingCart';
+import IconProfile from '../icons/IconProfile';
+import LoginButton from '../LoginButton';
+import LogoutButton from '../LogoutButton';
+
 function Header() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className={styles.header}>
-      <Link to="/">Logo</Link>
-      <div>Navbar</div>
-      <div>
-        <Link to="/menus">Menus</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/registro">Registro</Link>
+      <div className={styles.logo}>
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+
+      <ul className={styles.listLinks}>
+        <li>
+          <Link to="/">Nosotros</Link>
+        </li>
+        <li>
+          <Link to="/">Como Comprar?</Link>
+        </li>
+        <li>
+          <Link to="/menus">Productos</Link>
+        </li>
+      </ul>
+
+      <div className={styles.menu}>
+        <Link to="/buscar">
+          <IconSearch />
+        </Link>
+        <Link to="/carrito">
+          <IconShopingCart />
+        </Link>
+        <Link to="/perfil">
+          <IconProfile />
+        </Link>
+        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
       </div>
     </div>
   );
