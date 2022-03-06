e954e9258c7b5ed6dc63d75cbb228cc695939474
Markos <markosmk@me.com>
2022-03-05 21:13:55 -0300

    Add icons components

---
+++ frontend/src/components/icons/IconProfile.js
@@ -0,0 +1,26 @@
+function IconProfile() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      width="1em"
+      height="1em"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      >
+        <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2Z" />
+        <path d="M4.271 18.346S6.5 15.5 12 15.5s7.73 2.846 7.73 2.846M12 12a3 3 0 1 0 0-6a3 3 0 0 0 0 6Z" />
+      </g>
+    </svg>
+  );
+}
+
+export default IconProfile;
---
+++ frontend/src/components/icons/IconSearch.js
@@ -0,0 +1,20 @@
+function IconSearch() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      width="1em"
+      height="1em"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 256 256"
+    >
+      <path
+        fill="currentColor"
+        d="m232.5 215.5l-40.7-40.7A94.9 94.9 0 0 0 212 116a96 96 0 1 0-96 96a94.9 94.9 0 0 0 58.8-20.2l40.7 40.7a12.1 12.1 0 0 0 17 0a12 12 0 0 0 0-17ZM44 116a72 72 0 1 1 72 72a72.1 72.1 0 0 1-72-72Z"
+      />
+    </svg>
+  );
+}
+
+export default IconSearch;
