631b386a5de3e6aa5fe2c27d79f53d657f4f300f
Santiago Perez <santy.p510@gmail.com>
2022-03-18 02:56:52 -0300

    Added Product managment into MainController. It's a trial.

--- Santero/src/main/java/com/Santero/controllers/MainController.java
+++ Santero/src/main/java/com/Santero/controllers/MainController.java
@@ -1,12 +1,18 @@
 package com.Santero.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.Santero.entities.Product;
 import com.Santero.services.ClientOrderService;
 import com.Santero.services.ClientService;
 import com.Santero.services.PaymentService;
@@ -46,5 +52,24 @@ public class MainController {
 			System.err.println(e.getMessage());
 			return "redirect:/admin";
 		}
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	@PostMapping("/product_save_and_edit")
+	public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+		Product obj = productService.saveProduct(product);
+		return new ResponseEntity<Product>(obj, HttpStatus.OK);
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	@GetMapping("/delete/{idProduct}")
+	public ResponseEntity<Product> deleteProduct(@PathVariable String idProduct) {
+		Product product = productService.getById(idProduct);
+		if(product != null) {
+			productService.deleteProduct(idProduct);
+		}else {			
+			return new ResponseEntity<Product>(product, HttpStatus.INTERNAL_SERVER_ERROR);
+		}		
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
 	}
 }
