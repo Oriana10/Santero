9ae5f45bc8cf1925ccf285e58033b7e7033ef01a
Mark <markosmk@me.com>
2022-05-14 15:20:08 -0300

    Initial features base (#59)
    * add: new base project styles
    * update: minor changes in container
    * update: minor changes in Header
    * update: added font Poppins

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom';
+// import Navbar from './Navbar/Navbar';
+
 import styles from './Header.module.css';
 
 function Header() {
   return (
     <div className={styles.header}>
-      <Link to="/">Logo</Link>
-      <div>Navbar</div>
-      <div>
-        <Link to="/menus">Menus</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/registro">Registro</Link>
-      </div>
+      {/* <Navbar /> */}
+      {/** Hero */}
     </div>
   );
 }
